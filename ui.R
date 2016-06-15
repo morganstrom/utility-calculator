55a3d35e4ef94465ac24875770540b5d51afa1f8
morganstrom <morgan.e.strom@gmail.com>
2016-06-15 13:11:08 +0100

    Slight change to the text

--- ui.R
+++ ui.R
@@ -146,9 +146,9 @@ shinyUI(
         br(),
         HTML("Where \\(A\\) is the event that a given candidate will perform well on the job (i.e. ends up above the horizontal line of the graph) and 
              \\(B\\) is the event that a given candidate performs well on the assessment (i.e ends up to the right of the vertical line of the graph).
-             The conditional probability \\(P(A \\mid B)\\) can be calculated exactly using properties of the 
+             The conditional probability \\(P(A \\mid B)\\) can be calculated using properties of the 
              <a href='https://en.wikipedia.org/wiki/Multivariate_normal_distribution#Bivariate_case'>bivariate normal distribution function</a>. 
-             If you want to control the results yourself, you should get similar results by dividing the number of green points with the 
+             If you want to check the results, you should get similar results by dividing the number of green points with the 
              total number of green and red points from the simulation above."),
         br(),
         br(), 
